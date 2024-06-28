34473b2ac0fe1c8a1dd47ae65e23eb1e54e79347
Gervis Bermudez <gervisbermudez@outlook.com>
2024-06-28 13:29:40 -0300

    update github actions yml

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@
 const nextConfig = {
   // Agregar configuraciones adicionales si es necesario
   output: "export", // Habilitar la exportación estática
-  assetPrefix: process.env.NODE_ENV === "production" ? "/gervisbermudez-blog/" : "", // Ruta base para GitHub Pages
+  basePath: process.env.NODE_ENV === "production" ? "/gervisbermudez-blog/" : "", // Ruta base para GitHub Pages
   trailingSlash: true, // Agregar barra inclinada al final de cada ruta
 };
 
