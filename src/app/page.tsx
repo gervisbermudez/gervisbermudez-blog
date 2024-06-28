ba8aac3bb32f34838d63a0aac5dd169eec4ad5ea
Gervis Bermudez <gervisbermudez@outlook.com>
2024-06-28 13:32:47 -0300

    update home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div>
-      <h1>Bienvenido a mi Blog</h1>
+      <h1>Hola, este es mi nuevo blog! 😊</h1>
       <p>Este es un blog generado con Next.js</p>
     </div>
   );
