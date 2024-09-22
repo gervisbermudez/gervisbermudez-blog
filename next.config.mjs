2c7a19a453b82bab256f4ca42799fe44090ece1d
Gervis Bermudez <gervisbermudez@outlook.com>
2024-09-22 15:23:55 -0300

    adding files from original blog, removing next config

--- next.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  // Agregar configuraciones adicionales si es necesario
-  output: "export", // Habilitar la exportación estática
-  basePath: process.env.NODE_ENV === "production" ? "/gervisbermudez-blog" : "", // Ruta base para GitHub Pages
-  trailingSlash: true, // Agregar barra inclinada al final de cada ruta
-};
-
-export default nextConfig;
--- src/app/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div>
-      <h1>Hola, este es mi nuevo blog! 😊</h1>
-      <p>Este es un blog generado con Next.js</p>
-    </div>
-  );
-}
